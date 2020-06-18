f51d34b6483ee4af4fd3d15550bf19fb3ea26f82
Burak Tilek <burak.tilek@dreamapply.com>
2020-06-18 15:30:43 +0300

    Initial commit

---
+++ src/Jacs.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * JACS = Joint Academic Coding System
+ *
+ * This is a way of coding different academic subjects, used mainly in the
+ * United Kingdom, but we liked it so much that we use it internally.
+ *
+ * @see http://www.hesa.ac.uk/index.php?option=com_content&task=view&id=158&Itemid=233
+ *
+ * @package DreamLibrary
+ * @author Burak Tilek <burak.tilek@dreamapply.com>
+ *
+ */
+
+namespace Dream;
+
+use InvalidArgumentException;
+
+class Jacs
+{
+
+}
---
+++ tests/JacsTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests;
+
+class JacsTest extends \PHPUnit\Framework\TestCase
+{
+    
+}
+
